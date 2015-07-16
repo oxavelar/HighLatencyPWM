41f4a967fd185f066771c31a5361bed20330f5bd
Omar Avelar <omar.avelar@intel.com>
2015-07-16 17:00:44 -0500

    Set PWM methods in place.

--- PWM.cc
+++ PWM.cc
@@ -39,66 +39,57 @@ using std::endl;
 
 
 
-const std::string PWM::_sysfsPath("/sys/class/pwm/pwmchip0/");
+const std::string PWM::_sysfsPath("/sys/class/pwm/");
 
 
-PWM::PWM(unsigned short id, Direction direction) :
-   _id(id), _id_str(std::to_string(id)),
-   _direction(direction),
-   _edge(PWM::Edge::NONE),
-   _isr(std::function<void(Value)>()), // default constructor constructs empty function object
-   _pollThread(std::thread()),         // default constructor constructs non-joinable
-   _pollFD(-1),
-   _isrThread(std::thread()),          // default constructor constructs non-joinable
-   _destructing(false)
-
+PWM::PWM(unsigned short id) :
+   _id(id), _id_str(std::to_string(id))
 {
    initCommon();
 }
 
 
-PWM::PWM(unsigned short id, Edge edge, std::function<void(Value)> isr):
+PWM::PWM(unsigned short id, PWM::Dutycycle value, PWM::Period period_ns):
    _id(id), _id_str(std::to_string(id)),
-   _direction(PWM::Direction::IN),
-   _edge(edge),
-   _isr(isr),
-   _pollThread(std::thread()), // default constructor constructs non-joinable
-   _pollFD(-1),
-   _isrThread(std::thread()),  // default constructor constructs non-joinable
-   _destructing(false)
+   _dutycycle(value),
+   _period_ns(period_ns)
 {
-   initCommon();
 
-   //attempt to set edge detection
+   /* Attempt to set the dutycycle */
    {
-      std::ofstream sysfs_edge(_sysfsPath + "gpio" + _id_str + "/edge", std::ofstream::app);
-      if( !sysfs_edge.is_open() )
+      std::ofstream sysfs_dutycycle(_sysfsPath + "pwm" + _id_str + "/duty_cycle", std::ofstream::app);
+      if( !sysfs_dutycycle.is_open() )
       {
          throw std::runtime_error(
-            "Unable to set edge for PWM " + _id_str + "." +
-            "Are you sure this PWM can be configured for interrupts?");
+            "Unable to set duty cycle for PWM " + _id_str + "." +
+            "Are you sure this PWM channel can be duty cycle configured?");
       }
-      if     ( _edge == PWM::Edge::NONE )    sysfs_edge << "none";
-      else if( _edge == PWM::Edge::RISING )  sysfs_edge << "rising";
-      else if( _edge == PWM::Edge::FALLING ) sysfs_edge << "falling";
-      else if( _edge == PWM::Edge::BOTH )    sysfs_edge << "both";
-      sysfs_edge.close();
+      /* Set the numerical string value */
+      sysfs_dutycycle << std::to_string(_dutycycle);
+      sysfs_dutycycle.close();
    }
 
+   /* Attempt to set the period */
+   {
+      std::ofstream sysfs_period(_sysfsPath + "pwm" + _id_str + "/period", std::ofstream::app);
+      if( !sysfs_period.is_open() )
+      {
+         throw std::runtime_error(
+            "Unable to set period for PWM " + _id_str + "." +
+            "Are you sure this PWM channel can be period configured?");
+      }
+      /* Set the numerical string value */
+      sysfs_period << std::to_string(_period_ns);
+      sysfs_period.close();
+   }
 
-   // It is valid to use the this pointer in the constructor in this case
-   // http://www.parashift.com/c++-faq/using-this-in-ctors.html
-   _isrThread = std::thread(&PWM::isrLoop, this);
-
-   _pollThread = std::thread(&PWM::pollLoop, this);
-
-   sched_yield();
+   initCommon();
 }
 
 
-void PWM::initCommon() const
+void PWM::initCommon(void) const
 {
-   //validate id #
+   /* validate id # */
    {
       if( !boost::filesystem::exists(_sysfsPath) )
       {
@@ -106,13 +97,13 @@ void PWM::initCommon() const
       }
 
       using boost::filesystem::directory_iterator;
-      const directory_iterator end_itr; // default construction yields past-the-end
+      const directory_iterator end_itr; /* default construction yields past-the-end */
 
       bool found = false;
       for( directory_iterator itr(_sysfsPath); itr != end_itr; ++itr)
       {
          if( is_directory(itr->status()) &&
-             itr->path().string().find("gpiochip") != std::string::npos )
+             itr->path().string().find("pwmchip") != std::string::npos )
          {
             std::ifstream infile(itr->path().string() + "/base");
             if( !infile )
@@ -120,24 +111,26 @@ void PWM::initCommon() const
                throw std::runtime_error("Unable to read  " + itr->path().string() + "/base");
             }
 
-            // There is no way to be fast and const-correct here :(
-            // http://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html
+            /*
+               There is no way to be fast and const-correct here :(
+               http://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html
+            */
             std::string base;
             infile >> base;
             infile.close();
 
-            infile.open(itr->path().string() + "/ngpio");
+            infile.open(itr->path().string() + "/npwm");
             if( !infile )
             {
-               throw std::runtime_error("Unable to read  " + itr->path().string() + "/ngpio");
+               throw std::runtime_error("Unable to read  " + itr->path().string() + "/npwm");
             }
 
-            std::string ngpio;
-            infile >> ngpio;
+            std::string npwm;
+            infile >> npwm;
             infile.close();
 
-            // PWM id is valid
-            if( std::stoul(base) <= _id && _id < std::stoul(base) + std::stoul(ngpio) )
+            /* PWM id is valid */
+            if( std::stoul(base) <= _id && _id < std::stoul(base) + std::stoul(npwm) )
             {
                found = true;
                break;
@@ -152,11 +145,11 @@ void PWM::initCommon() const
 
 
 
-   // validate not already exported
+   /* validate not already exported */
    {
-      // In decreasing order of speed: stat() -> access() -> fopen() -> ifstream
+      /* In decreasing order of speed: stat() -> access() -> fopen() -> ifstream */
       struct stat stat_buf;
-      const std::string path(_sysfsPath + "gpio" + _id_str);
+      const std::string path(_sysfsPath + "pwm" + _id_str);
       if( stat(path.c_str() , &stat_buf) == 0 )
       {
          throw std::runtime_error(
@@ -167,7 +160,7 @@ void PWM::initCommon() const
 
 
 
-   // attempt to export
+   /* attempt to export */
    {
       std::ofstream sysfs_export(_sysfsPath + "export", std::ofstream::app);
       if( !sysfs_export.is_open() )
@@ -180,239 +173,23 @@ void PWM::initCommon() const
 
 
 
-   //attempt to set direction
+   /* attempt to set disable by default */
    {
-      std::ofstream sysfs_direction(
-         _sysfsPath + "gpio" + _id_str + "/direction", std::ofstream::app);
-      if( !sysfs_direction.is_open() )
+      std::ofstream sysfs_enable(
+         _sysfsPath + "gpio" + _id_str + "/enable", std::ofstream::app);
+      if( !sysfs_enable.is_open() )
       {
-         throw std::runtime_error("Unable to set direction for PWM " + _id_str);
+         throw std::runtime_error("Unable to disable PWM " + _id_str);
       }
-      if( _direction == PWM::Direction::IN )        sysfs_direction << "in";
-      else if( _direction == PWM::Direction::OUT )  sysfs_direction << "out";
-      sysfs_direction.close();
-   }
-
-
-
-   //attempt to clear active low
-   {
-      std::ofstream sysfs_activelow(_sysfsPath + "gpio" + _id_str + "/active_low", std::ofstream::app);
-      if( !sysfs_activelow.is_open() )
-      {
-         throw std::runtime_error("Unable to clear active_low for PWM " + _id_str);
-      }
-      sysfs_activelow << "0";
-      sysfs_activelow.close();
-   }
-
-
-
-   //if output, set value to inactive
-   {
-      if( _direction == PWM::Direction::OUT )
-      {
-         std::ofstream sysfs_value(_sysfsPath + "gpio" + _id_str + "/value", std::ofstream::app);
-         if( !sysfs_value.is_open() )
-         {
-            throw std::runtime_error("Unable to initialize value for PWM " + _id_str);
-         }
-         sysfs_value << "0";
-         sysfs_value.close();
-      }
+      if( _state == PWM::State::DISABLED )      sysfs_enable << "0";
+      else if( _state == PWM::State::ENABLED )  sysfs_enable << "1";
+      sysfs_enable.close();
    }
 }
-
-
-void PWM::pollLoop()
-{
-   // No easy way to get file descriptor from ifstream... ugh.
-   {
-      const std::string path(_sysfsPath + "gpio" + _id_str + "/value");
-      _pollFD = open(path.c_str(), O_RDONLY | O_NONBLOCK); // closed in destructor
-      if( _pollFD < 0 )
-      {
-         perror("open");
-         throw std::runtime_error("Unable to open " + path);
-      }
-   }
-
-   // There is no way to have poll() come out of a blocking state except when it detects activity on
-   // file descriptors it is monitoring, or when a process/thread blocked in poll() receives a
-   // signal. Because this thread will not terminate unless poll() comes out of a blocking state at
-   // some point, we need a mechanism to kick poll() out of its blocking state, even if no activity
-   // is detected on the file descriptor of interest, _pollFD. I have chosen to use a pipe to
-   // to acquire a stream type file descriptor which poll() will monitor for activity. I will not
-   // cause any activity to occur on this file descriptor until I wish to terminate the thread.
-   {
-      if( pipe(_pipeFD) != 0 )
-      {
-         perror("pipe");
-         throw std::runtime_error("Unable to create pipe");
-      }
-   }
-
-
-   const int MAX_BUF = 2; // either 1 or 0 plus EOL
-   char buf[MAX_BUF];
-   struct pollfd fdset[2];
-
-   memset((void*)fdset, 0, sizeof(fdset));
-
-   fdset[0].fd     = _pollFD;
-   fdset[0].events = POLLPRI;
-
-   fdset[1].fd     = _pipeFD[0]; // This is the FD for the read end of the pipe
-   fdset[1].events = POLLRDHUP; // Monitor for closed connection
-
-   /// Consume the initial value
-   {
-      const ssize_t nbytes = read(fdset[0].fd, buf, MAX_BUF);
-      if( nbytes != MAX_BUF )
-      {
-         // It is possible that read() could:
-         //  return 1 (which could be recovered from)
-         //  return 0 (which could be recovered from in the case no errors are detected)
-         //  return < 0 (which could not be recovered from)
-         // I suspect these cases are extraordinarily rare, and do not currently consider them to be
-         // worth the amount of code necessary to gracefully recover, or the possibility of
-         // introducing bugs in that code. I'm willing to be wrong; just contact me if you see the
-         // error below, want to make the argument that the code is necessary, or can provide said
-         // code. :) This also applies to the read() in the loop below.
-         if( nbytes < 0 ) perror("read1");
-         throw std::runtime_error("PWM " + _id_str + " read1() badness...");
-      }
-   }
-
-
-
-
-   while(1)
-   {
-      const int rc = poll(fdset, 2, -1);
-      if( rc == 1 )
-      {
-         if(fdset[0].revents & POLLPRI)
-         {
-            /// Consume the new value
-            lseek(fdset[0].fd, 0, SEEK_SET);
-            const ssize_t nbytes = read(fdset[0].fd, buf, MAX_BUF);
-            if( nbytes != MAX_BUF ) // See comment above
-            {
-
-               if( nbytes < 0 )
-                  perror("read2");
-
-               throw std::runtime_error("PWM " + _id_str + " read2() badness...");
-            }
-
-
-            Value val;
-            if     ( buf[0] == '0' )  val = PWM::Value::LOW;
-            else if( buf[0] == '1' )  val = PWM::Value::HIGH;
-            else throw std::runtime_error("Invalid value read from PWM " + _id_str + ": " + buf[0]);
-
-   #ifdef LOCKFREE
-            while( !_spsc_queue.push(val) )
-               ;
-   #else
-            {
-               std::lock_guard<std::mutex> lck(_eventMutex);
-               _eventQueue.push(val);
-               _eventCV.notify_one();
-            }
-   #endif
-
-         }
-         else // POLLRDHUP must have occurred, so end the thread
-         { return; }
-      }
-      else if( rc < 0 )
-      {
-         if( errno == EINTR )
-            continue;
-
-         perror("poll");
-         throw std::runtime_error("poll() error on PWM " + _id_str);
-      }
-      else if( rc == 0 )
-      {
-         using std::runtime_error;
-         throw runtime_error("poll() return code indicates timeout, which should never happen.");
-      }
-      else if( rc > 1 ) // POLLRDHUP must have occurred, so end the thread
-      { return; }
-   }
-}
-
-// Process interrupt events serially
-void PWM::isrLoop()
-{
-   Value val;
-
-   while(1)
-   {
-#ifdef LOCKFREE
-      //!***************************************** BEWARE ****************************************!/
-      /// On the BeagleBone, this loop is effectively a spinlock. Unless there is a lot of PWM
-      /// activity it will be EXTREMELY wasteful of CPU time!!! (It is guaranteed to waste an entire
-      /// quantum if not immediately successful at obtaining a value.) On Multicore systems this
-      /// approach should provide slightly lower latency than mutexes and condition variables at the
-      /// expense of CPU time. On the BeagleBone Black, it provides about 0.5 ms lower latency, but
-      /// nowhere near what the BeagleBone Black PRUs can provide (nanoseconds), or even what a
-      /// kernel module can provide (microseconds).
-      //!*****************************************************************************************!/
-      while( !_spsc_queue.pop(val) )
-         if( _destructing )
-            return;
-#else
-      std::unique_lock<std::mutex> lck(_eventMutex);
-      while( _eventQueue.empty() )
-      {
-         _eventCV.wait(lck);
-
-         if( _destructing == true )
-            return;
-      }
-
-
-      val = _eventQueue.front();
-      _eventQueue.pop();
-      lck.unlock();
-#endif
-
-      /// *************************************************************
-      /// If this (user) function causes an exception to be thrown,
-      /// it will not be handled or ignored!!!
-      /// *************************************************************
-      _isr(val);
-   }
-}
-
-
 
 PWM::~PWM()
 {
-   // Set this flag to true in order to indicate to _isrThread that it needs to terminate
-   _destructing = true;
-#ifndef LOCKFREE
-   _eventCV.notify_one();
-#endif
-
-   // Close the file descriptors for this pipe to trigger a POLLRDHUP event, which will cause
-   // _pollThread to terminate
-   close(_pipeFD[0]);
-   close(_pipeFD[1]);
-
-   if( _isrThread.joinable())   _isrThread.join();
-   if( _pollThread.joinable())  _pollThread.join();
-
-   // Do not close the file descriptor for the sysfs value file until _pollThread() has joined.
-   // This prevents reuse of this file descriptor by the kernel for other threads in this
-   // process while the descriptor is still in use in the poll() system call.
-   close(_pollFD);
-
-   // attempt to unexport
+   /* attempt to unexport */
    try
    {
       std::ofstream sysfs_unexport(_sysfsPath + "unexport", std::ofstream::app);
@@ -421,7 +198,7 @@ PWM::~PWM()
          sysfs_unexport << _id_str;
          sysfs_unexport.close();
       }
-      else // Do not throw exception in destructor! Effective C++ Item 8.
+      else /* Do not throw exception in destructor! Effective C++ Item 8. */
       {
          cerr << "Unable to unexport PWM " + _id_str + "!" << endl;
          cerr << "This will prevent initialization of another PWM object for this PWM." << endl;
@@ -434,43 +211,42 @@ PWM::~PWM()
 }
 
 
-void PWM::setValue(const Value value) const
+void PWM::setDutyCycle(const PWM::Dutycycle &value)
 {
-   if( _direction == PWM::Direction::IN )
+   if( (value > 100) or (value < 0) )
    {
-      throw std::runtime_error("Cannot set value on an input PWM");
+      throw std::runtime_error("Pick a value between 0 and 100 for the PWM dutycycle");
    }
 
-   std::ofstream sysfs_value(_sysfsPath + "gpio" + _id_str + "/value", std::ofstream::app);
+   std::ofstream sysfs_value(_sysfsPath + "pwm" + _id_str + "/duty_cycle", std::ofstream::app);
    if( !sysfs_value.is_open() )
    {
-      throw std::runtime_error("Unable to set value for PWM " + _id_str);
+      throw std::runtime_error("Unable to set dutycycle value for PWM " + _id_str);
    }
 
-   if     ( value == PWM::Value::HIGH )  sysfs_value << "1";
-   else if( value == PWM::Value::LOW )   sysfs_value << "0";
+   sysfs_value << std::to_string(value);
    sysfs_value.close();
+   _dutycycle = value;
 }
 
 
-PWM::Value PWM::getValue() const
+PWM::Dutycycle PWM::getDutyCycle(void)
 {
-   std::ifstream sysfs_value(_sysfsPath + "gpio" + _id_str + "/value");
+   std::ifstream sysfs_value(_sysfsPath + "pwm" + _id_str + "/duty_cycle");
    if( !sysfs_value.is_open() )
    {
-      throw std::runtime_error("Unable to get value for PWM " + _id_str);
+      throw std::runtime_error("Unable to get dutycycle value for PWM " + _id_str);
    }
 
    const char value = sysfs_value.get();
    if( !sysfs_value.good() )
    {
-      throw std::runtime_error("Unable to get value for PWM " + _id_str);
+      throw std::runtime_error("Unable to get dutycycle value for PWM " + _id_str);
    }
 
-   Value val;
-   if     ( value == '0' )  val = PWM::Value::LOW;
-   else if( value == '1' )  val = PWM::Value::HIGH;
-   else throw std::runtime_error("Invalid value read from PWM " + _id_str + ": " + value);
+   Dutycycle val = value - '0';
+   _dutycycle = val;
 
    return(val);
 }
+
--- PWM.hh
+++ PWM.hh
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2014 Thomas Mercier Jr.
+Copyright (c) 2014 Thomas Mercier Jr & Omar X. Avelar.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,8 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#ifndef PWM_HH
-#define PWM_HH
+#ifndef GPIO_HH
+#define GPIO_HH
 
 #include "Uncopyable.hh"
 
@@ -32,154 +32,37 @@ SOFTWARE.
 #include <string>
 #include <thread>
 
-// LOCKFREE define specifies the use of a (single producer, single consumer) lockfree container for
-// the transfer of transition events from the thread which detects these events, to the thread which
-// will call the user-provided callback function. This implementation is EXTREMELY wasteful of CPU
-// time!!! However, it provides about 0.5 ms lower latency than the default implementation. If lower
-// latency is required, please one of the BeagleBone Black PRUs, or a kernel module.
-#ifdef LOCKFREE
-   #include <boost/lockfree/spsc_queue.hpp>
-#else
-   #include <queue>
-   #include <mutex>
-   #include <condition_variable>
-#endif
-
 
 class PWM : private Uncopyable
 {
 public:
+    
+    enum class State : char { DISABLED, ENABLED };
 
-   //-----------------------------------------------------------------------------------------------
-   /// @enum Direction
-   /// @brief Type used to configure a PWM as an input or output
-   //-----------------------------------------------------------------------------------------------
-   enum class Direction : char {
-      IN,
-      OUT
-   };
+    using Dutycycle = long;
+    using Period = long;
+    
+    explicit PWM(unsigned short id);
+    explicit PWM(unsigned short id, Dutycycle value, Period period_ns);
+    virtual ~PWM(void);
 
-   //-----------------------------------------------------------------------------------------------
-   /// @enum Value
-   /// @brief Type used to indicate the logic level of a PWM
-   //-----------------------------------------------------------------------------------------------
-   enum class Value : char {
-      HIGH,
-      LOW
-   };
+    void setDutyCycle(const Dutycycle &value);
+    void setPeriod(const Period &period_ns);
 
-   //-----------------------------------------------------------------------------------------------
-   /// @enum Edge
-   /// @brief Type used to indicate which logic level transitions on an input PWM should result in
-   ///        a call to the user-provided callback function.
-   //-----------------------------------------------------------------------------------------------
-   enum class Edge : char {
-      NONE,
-      RISING,
-      FALLING,
-      BOTH
-   };
-
-
-   //-----------------------------------------------------------------------------------------------
-   // FUNCTION NAME: PWM (constructor)
-   ///
-   /// @brief Construt an input or output PWM object.
-   ///
-   /// @param[in]   id         The PWM ID. Often referred to a "pin number".
-   /// @param[in]   direction  The type (INPUT or OUTPUT) of PWM to construct.
-   ///
-   //-----------------------------------------------------------------------------------------------
-   explicit PWM(
-      unsigned short id,
-      Direction direction);
-
-
-   //-----------------------------------------------------------------------------------------------
-   // FUNCTION NAME: PWM (constructor)
-   ///
-   /// @brief Construt an input PWM object which will call a callback function every time a
-   ///        transition of type edge occurs.
-   ///
-   ///
-   /// @param[in]   id    The PWM ID. Often referred to a "pin number".
-   /// @param[in]   edge  The type (INPUT or OUTPUT) of PWM to construct.
-   /// @param[in]   isr   The function to call when transitions of type edge occur.
-   ///
-   /// @note If function isr throws an exception, IT WILL NOT BE HANDLED OR IGNORED BY THIS CLASS.
-   ///
-   //-----------------------------------------------------------------------------------------------
-   explicit PWM(
-      unsigned short id,
-      Edge edge,
-      std::function<void(Value)> isr);
-
-
-   //-----------------------------------------------------------------------------------------------
-   // FUNCTION NAME: PWM (destructor)
-   ///
-   /// @note If a program which uses this class is ungracefully terminated (this destructor is not
-   ///       called), the PWM will be left in an exported state which will prevent subsequent
-   ///       construction of a PWM object with the same id.
-   //-----------------------------------------------------------------------------------------------
-   ~PWM();
-
-
-   //-----------------------------------------------------------------------------------------------
-   // FUNCTION NAME: setValue
-   ///
-   /// @brief Set the logical value (HIGH or LOW) of the PWM. All PWMs are active-high.
-   ///
-   /// @param[in]   value    The logical value to set.
-   ///
-   /// @return None
-   ///
-   //-----------------------------------------------------------------------------------------------
-   void setValue(const Value value) const;
-
-
-   //-----------------------------------------------------------------------------------------------
-   // FUNCTION NAME: getValue
-   ///
-   /// @brief Get the logical value (HIGH or LOW) of the PWM. All PWMs are active-high.
-   ///
-   /// @return The logical value of the PWM.
-   ///
-   //-----------------------------------------------------------------------------------------------
-   Value getValue() const;
+    Dutycycle getDutyCycle(void);
+    Period getPeriod(void);
 
 
 private:
-   void initCommon() const;
-   void pollLoop();
-   void isrLoop();
+    static const std::string  _sysfsPath;
 
-private:
-   static const std::string  _sysfsPath;
+    const unsigned short _id;
+    const std::string _id_str;
+    Period _period_ns;
+    Dutycycle _dutycycle;
+    State _state = State::DISABLED;
 
-   const unsigned short _id;
-   const std::string    _id_str;
-   const Direction      _direction;
-
-   const Edge _edge;
-   const std::function<void(Value)> _isr;
-
-   std::thread _pollThread;
-   int _pollFD;
-
-   std::thread _isrThread;
-
-   std::atomic<bool> _destructing;
-   int               _pipeFD[2];
-
-#ifdef LOCKFREE
-   boost::lockfree::spsc_queue<Value, boost::lockfree::capacity<64>> _spsc_queue;
-#else
-   std::queue<Value>        _eventQueue; // stores values generated by interrupts
-   std::mutex               _eventMutex;
-   std::condition_variable  _eventCV;
-#endif
-
+    void initCommon(void) const;
 };
 
 #endif
